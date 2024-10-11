43f0ca0e4b083d1dea05e8e11e84e0b0a704e168
helonely <idravil1211@gmail.com>
2024-10-12 02:32:15 +0500

    added PaymentSerializer and payment_history

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
+class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = Payment
         fields = "__all__"
 
 
+class UserSerializer(serializers.ModelSerializer):
+    payment_history = PaymentSerializer(source='payment_set', many=True)
 
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'payment_history', 'password',)
