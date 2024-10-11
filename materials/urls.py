28ddca9b95d49393f96e7ad3f5595a37a7ab4353
helonely <idravil1211@gmail.com>
2024-10-12 02:34:00 +0500

    fix pep8

--- materials/urls.py
+++ materials/urls.py
@@ -2,28 +2,33 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import (CourseViewSet, LessonCreateAPIView,
-                             LessonDestroyAPIView, LessonListAPIView,
-                             LessonRetrieveAPIView, LessonUpdateAPIView)
+from materials.views import (
+    CourseViewSet,
+    LessonCreateAPIView,
+    LessonDestroyAPIView,
+    LessonListAPIView,
+    LessonRetrieveAPIView,
+    LessonUpdateAPIView,
+)
 
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
-router.register(r"courses", viewset=CourseViewSet, basename="courses")
+router.register("", viewset=CourseViewSet, basename="materials")
 
 
 urlpatterns = [
-    path("lessons/create", LessonCreateAPIView.as_view(), name="lesson-create"),
-    path("lessons/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lessons/", LessonListAPIView.as_view(), name="lesson_list"),
     path(
         "lessons/<int:pk>/retrieve",
         LessonRetrieveAPIView.as_view(),
         name="lesson-retrieve",
     ),
+    path("lessons/create", LessonCreateAPIView.as_view(), name="lesson_create"),
     path(
-        "lessons/<int:pk>/update", LessonUpdateAPIView.as_view(), name="lesson-update"
+        "lessons/<int:pk>/update", LessonUpdateAPIView.as_view(), name="lesson_update"
     ),
     path(
-        "lessons/<int:pk>/delete", LessonDestroyAPIView.as_view(), name="lesson-delete"
+        "lessons/<int:pk>/delete", LessonDestroyAPIView.as_view(), name="lesson_delete"
     ),
 ] + router.urls
