83fc566f2c20d435d3f201a96ce654d41938a38c
helonely <idravil1211@gmail.com>
2024-10-12 19:43:25 +0500

    added IsModer & IsOwner

---
+++ users/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+
+class IsModer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='moders').exists()
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
