a3055e105d013e2a20377deda7e08720de9257cd
helonely <idravil1211@gmail.com>
2024-10-12 15:24:47 +0500

    added perform_create

--- users/views.py
+++ users/views.py
@@ -9,6 +9,10 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
