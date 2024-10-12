8a48de85e141bbaaca9639e8c2abe2f60d407019
helonely <idravil1211@gmail.com>
2024-10-12 19:47:20 +0500

    added get_permissions

--- users/views.py
+++ users/views.py
@@ -1,18 +1,25 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, generics, filters
+from rest_framework import filters, generics, viewsets
+from rest_framework.permissions import AllowAny
 
-from users.models import User, Payment
-from users.serializers import UserSerializer, PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
+
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
+
 
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
