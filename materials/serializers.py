063ec77977bdfd5a67e77a6449725554bf2f2bd1
helonely <idravil1211@gmail.com>
2024-10-19 22:41:23 +0500

    added validators

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
+from materials.validators import LinkValidator
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -21,6 +22,9 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [
+            LinkValidator(field='video_url'),
+        ]
 
 
 class CourseCountSerializer(serializers.ModelSerializer):
